37abcb0f5b2cfbc55e8229f219cd83a91ed416cd
wzsdch <wzsdch@qq.com>
2019-10-24 17:42:22 +0800

    weicheng

--- src/main/java/com/ldxx/LqtProjectApplication.java
+++ src/main/java/com/ldxx/LqtProjectApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LqtProjectApplication {
-
-	public static void main(String[] args) { 
+	public static void main(String[] args) {
 		SpringApplication.run(LqtProjectApplication.class, args);
+        System.out.println(1);
 	}
 }
