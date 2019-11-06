6ba6b3e6acad407ad6ab8d94af012756929675b1
wzsdch <wzsdch@qq.com>
2019-11-06 14:58:15 +0800

    weicheng

---
+++ src/main/java/com/ldxx/service/SysFormulationManagementService.java
@@ -0,0 +1,18 @@
+package com.ldxx.service;
+
+import java.util.List;
+
+import com.ldxx.bean.SysFormulationManagement;
+
+public interface SysFormulationManagementService {
+
+	public int addSysFormulationManagement(SysFormulationManagement sysFormulationManagement);
+	
+	public int updateSysFormulationManagement(SysFormulationManagement sysFormulationManagement);
+	
+	public int deleteSysFormulationManagement(String id);
+	
+	public List<SysFormulationManagement> selectAllSysFormulationManagement();
+	
+	public SysFormulationManagement selectByIdSysFormulationManagement();
+}
