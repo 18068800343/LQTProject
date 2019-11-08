a96228e56474684682e5094cf98af48494491c45
wzsdch <wzsdch@qq.com>
2019-11-08 14:18:06 +0800

    weicheng

---
+++ src/main/java/com/ldxx/bean/SysMaterialAttached.java
@@ -0,0 +1,28 @@
+package com.ldxx.bean;
+
+import java.math.BigDecimal;
+
+public class SysMaterialAttached {
+	private String flId;
+	private String mName;
+	private BigDecimal weight;
+	public String getFlId() {
+		return flId;
+	}
+	public void setFlId(String flId) {
+		this.flId = flId;
+	}
+	public String getmName() {
+		return mName;
+	}
+	public void setmName(String mName) {
+		this.mName = mName;
+	}
+	public BigDecimal getWeight() {
+		return weight;
+	}
+	public void setWeight(BigDecimal weight) {
+		this.weight = weight;
+	}
+
+}
