e85cc6f3fb3ca71c19f55dae21c580995dfed7b1
wzsdch <wzsdch@qq.com>
2019-11-08 16:30:58 +0800

    weicheng

--- src/main/java/com/ldxx/bean/SysFormulationManagement.java
+++ src/main/java/com/ldxx/bean/SysFormulationManagement.java
@@ -1,18 +1,21 @@
 package com.ldxx.bean;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 public class SysFormulationManagement {
 	private String flId;
 	private String flNo;
 	private String flName;
-	private Double materialsUse1;
-	private Double materialsUse2;
-	private Double materialsUse3;
-	private Double materialsUse4;
-	private Double materialsUse5;
-	private Double materialsUse6;
-	private Double boneFlourUse;
-	private Double pitchUse;
-	private Double otherUse;
+	private BigDecimal materialsUse1;
+	private BigDecimal materialsUse2;
+	private BigDecimal materialsUse3;
+	private BigDecimal materialsUse4;
+	private BigDecimal materialsUse5;
+	private BigDecimal materialsUse6;
+	private BigDecimal boneFlourUse;
+	private BigDecimal pitchUse;
+	private List<SysMaterialAttached> fsclList;
 	private Integer deleteState;
 	public String getFlId() {
 		return flId;
@@ -32,59 +35,53 @@ public class SysFormulationManagement {
 	public void setFlName(String flName) {
 		this.flName = flName;
 	}
-	public Double getMaterialsUse1() {
+	public BigDecimal getMaterialsUse1() {
 		return materialsUse1;
 	}
-	public void setMaterialsUse1(Double materialsUse1) {
+	public void setMaterialsUse1(BigDecimal materialsUse1) {
 		this.materialsUse1 = materialsUse1;
 	}
-	public Double getMaterialsUse2() {
+	public BigDecimal getMaterialsUse2() {
 		return materialsUse2;
 	}
-	public void setMaterialsUse2(Double materialsUse2) {
+	public void setMaterialsUse2(BigDecimal materialsUse2) {
 		this.materialsUse2 = materialsUse2;
 	}
-	public Double getMaterialsUse3() {
+	public BigDecimal getMaterialsUse3() {
 		return materialsUse3;
 	}
-	public void setMaterialsUse3(Double materialsUse3) {
+	public void setMaterialsUse3(BigDecimal materialsUse3) {
 		this.materialsUse3 = materialsUse3;
 	}
-	public Double getMaterialsUse4() {
+	public BigDecimal getMaterialsUse4() {
 		return materialsUse4;
 	}
-	public void setMaterialsUse4(Double materialsUse4) {
+	public void setMaterialsUse4(BigDecimal materialsUse4) {
 		this.materialsUse4 = materialsUse4;
 	}
-	public Double getMaterialsUse5() {
+	public BigDecimal getMaterialsUse5() {
 		return materialsUse5;
 	}
-	public void setMaterialsUse5(Double materialsUse5) {
+	public void setMaterialsUse5(BigDecimal materialsUse5) {
 		this.materialsUse5 = materialsUse5;
 	}
-	public Double getMaterialsUse6() {
+	public BigDecimal getMaterialsUse6() {
 		return materialsUse6;
 	}
-	public void setMaterialsUse6(Double materialsUse6) {
+	public void setMaterialsUse6(BigDecimal materialsUse6) {
 		this.materialsUse6 = materialsUse6;
 	}
-	public Double getBoneFlourUse() {
+	public BigDecimal getBoneFlourUse() {
 		return boneFlourUse;
 	}
-	public void setBoneFlourUse(Double boneFlourUse) {
+	public void setBoneFlourUse(BigDecimal boneFlourUse) {
 		this.boneFlourUse = boneFlourUse;
 	}
-	public Double getPitchUse() {
+	public BigDecimal getPitchUse() {
 		return pitchUse;
 	}
-	public void setPitchUse(Double pitchUse) {
+	public void setPitchUse(BigDecimal pitchUse) {
 		this.pitchUse = pitchUse;
-	}
-	public Double getOtherUse() {
-		return otherUse;
-	}
-	public void setOtherUse(Double otherUse) {
-		this.otherUse = otherUse;
 	}
 	public Integer getDeleteState() {
 		return deleteState;
@@ -92,5 +89,12 @@ public class SysFormulationManagement {
 	public void setDeleteState(Integer deleteState) {
 		this.deleteState = deleteState;
 	}
+	public List<SysMaterialAttached> getFsclList() {
+		return fsclList;
+	}
+	public void setFsclList(List<SysMaterialAttached> fsclList) {
+		this.fsclList = fsclList;
+	}
+
 	
 }
