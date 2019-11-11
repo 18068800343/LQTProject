aeeb07670f9d3cc9e9006e1f36cd3d1ffc794448
wzsdch <wzsdch@qq.com>
2019-11-11 11:09:29 +0800

    weicheng

--- src/main/java/com/ldxx/controller/SysFormulationManagementController.java
+++ src/main/java/com/ldxx/controller/SysFormulationManagementController.java
@@ -1,14 +1,21 @@
 package com.ldxx.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ldxx.bean.SysFormulationManagement;
+import com.ldxx.bean.SysMaterialAttached;
 import com.ldxx.service.SysFormulationManagementService;
+import com.ldxx.util.LDXXUtils;
+
 
 @Controller
 @RequestMapping("SysFormulationManagement")
@@ -19,16 +26,26 @@ public class SysFormulationManagementController {
 	
 	@RequestMapping("/addSysFormulationManagement")
 	@ResponseBody
-	public int addSysFormulationManagement(SysFormulationManagement sysFormulationManagement) {
-		int num = sfmService.addSysFormulationManagement(sysFormulationManagement);
-		return num;
+	public Map<String,Object> addSysFormulationManagement(@RequestBody SysFormulationManagement sysFormulationManagement) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		
+		int state=0;
+		sysFormulationManagement.setFlId(LDXXUtils.getUUID12());
+		state = sfmService.addSysFormulationManagement(sysFormulationManagement);
+		map.put("result",state);
+		map.put("SysFormulationManagement", sysFormulationManagement);
+		return map;
 	}
 	
 	@RequestMapping("/updateSysFormulationManagement")
 	@ResponseBody
-	public int updateSysFormulationManagement(SysFormulationManagement sysFormulationManagement) {
-		int num = sfmService.updateSysFormulationManagement(sysFormulationManagement);
-		return num;
+	public Map<String,Object> updateSysFormulationManagement(SysFormulationManagement sysFormulationManagement) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		sysFormulationManagement.setFlId(LDXXUtils.getUUID12());
+		int state = sfmService.updateSysFormulationManagement(sysFormulationManagement);
+		map.put("result",state);
+		map.put("SysFormulationManagement", sysFormulationManagement);
+		return map;
 	}
 	
 	@RequestMapping("/deleteSysFormulationManagement")
