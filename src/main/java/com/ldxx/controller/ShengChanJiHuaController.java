be2d6a13088f2ee09f6f65e5791b40c805fff4dd
夏宁 <1042449477@qq.com>
2019-11-15 10:16:22 +0800

    xn

--- src/main/java/com/ldxx/controller/ShengChanJiHuaController.java
+++ src/main/java/com/ldxx/controller/ShengChanJiHuaController.java
@@ -44,7 +44,6 @@ public class ShengChanJiHuaController {
 	@RequestMapping("/getAllShengChanJiHua")
 	@ResponseBody
     public List<PlanProductionCollectionVo> getAllShengChanJiHua() {
-
         return service.getShengChanJiHuaListByCondition();
     }
 	
