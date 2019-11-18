83bc017f9e4dc63d40129f30c033fecd077548d0
夏宁 <1042449477@qq.com>
2019-11-18 14:31:25 +0800

    xn

--- src/main/java/com/ldxx/bean/SiteIncomingMaterialTempWarning.java
+++ src/main/java/com/ldxx/bean/SiteIncomingMaterialTempWarning.java
@@ -1,4 +1,93 @@
 package com.ldxx.bean;
 
 public class SiteIncomingMaterialTempWarning {
+    private String id;
+
+    private String fieid;
+
+    private String datetime;
+
+    private String warningtype;
+
+    private String warningcontent;
+
+    private int warningstate;
+
+    private String edituserid;
+
+    private String editdatetime;
+
+    private int deletestate;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFieid() {
+        return fieid;
+    }
+
+    public void setFieid(String fieid) {
+        this.fieid = fieid;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getWarningtype() {
+        return warningtype;
+    }
+
+    public void setWarningtype(String warningtype) {
+        this.warningtype = warningtype;
+    }
+
+    public String getWarningcontent() {
+        return warningcontent;
+    }
+
+    public void setWarningcontent(String warningcontent) {
+        this.warningcontent = warningcontent;
+    }
+
+    public int getWarningstate() {
+        return warningstate;
+    }
+
+    public void setWarningstate(int warningstate) {
+        this.warningstate = warningstate;
+    }
+
+    public String getEdituserid() {
+        return edituserid;
+    }
+
+    public void setEdituserid(String edituserid) {
+        this.edituserid = edituserid;
+    }
+
+    public String getEditdatetime() {
+        return editdatetime;
+    }
+
+    public void setEditdatetime(String editdatetime) {
+        this.editdatetime = editdatetime;
+    }
+
+    public int getDeletestate() {
+        return deletestate;
+    }
+
+    public void setDeletestate(int deletestate) {
+        this.deletestate = deletestate;
+    }
 }
--- src/main/java/com/ldxx/dao/LaiLiaoWenDuYuJingDao.java
+++ src/main/java/com/ldxx/dao/LaiLiaoWenDuYuJingDao.java
@@ -1,20 +1,19 @@
 package com.ldxx.dao;
 
-import com.ldxx.vo.SiteFieldMaterialMgtVo;
 import com.ldxx.vo.SiteIncomingMaterialTempWarningVo;
 
 import java.util.List;
 
 public interface  LaiLiaoWenDuYuJingDao {
-	
-	
-	List<SiteIncomingMaterialTempWarningVo> getAllLaiLiaoWenDuYu();
 
-	
-	int addLaiLiaoWenDuYu(SiteFieldMaterialMgtVo siteFieldMaterialMgtVo);
 
-	int delLaiLiaoWenDuYu(String id);
-	
-	int updateLaiLiaoWenDuYu(SiteFieldMaterialMgtVo siteFieldMaterialMgtVo);
-	
+	List<SiteIncomingMaterialTempWarningVo> getAllLaiLiaoWenDu();
+
+
+	int addLaiLiaoWenDu(SiteIncomingMaterialTempWarningVo siteIncomingMaterialTempWarningVo);
+
+	int delLaiLiaoWenDu(String id);
+
+	int updateLaiLiaoWenDu(SiteIncomingMaterialTempWarningVo siteIncomingMaterialTempWarningVo);
+
 }
