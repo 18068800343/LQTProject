50582c35dbc09ce9c3f8ead30bfb050be0d47fbc
wzsdch <wzsdch@qq.com>
2019-11-18 19:10:44 +0800

    weicheng

--- src/main/java/com/ldxx/controller/USigninController.java
+++ src/main/java/com/ldxx/controller/USigninController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.ldxx.bean.USignin;
 import com.ldxx.service.USigninService;
@@ -16,6 +18,7 @@ public class USigninController {
 	USigninService usService;
 	
 	@RequestMapping("/selectAllUSignin")
+	@ResponseBody
 	public List<USignin> selectAllUSignin(){
 		List<USignin> usList = usService.selectAllUSignin();
 		return usList;
