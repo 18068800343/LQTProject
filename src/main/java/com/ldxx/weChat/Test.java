3038a47fa9d4ca9615ddd4398a17d1de66f82492
夏宁 <1042449477@qq.com>
2019-11-25 13:59:18 +0800

    xn

--- src/main/java/com/ldxx/weChat/Test.java
+++ src/main/java/com/ldxx/weChat/Test.java
@@ -5,13 +5,15 @@ public class Test {
     public static void main(String[] args) {
         WeChatMsgSend swx = new WeChatMsgSend();
         try {
-            //corpid 企业id
-            String token = swx.getToken("ww8dad70e28c6446b8","3H5uzjWkB80kpwP0Qj2rWeKyfZaY1ZiPOXFLUGOb_Fg");
-            String postdata = swx.createpostdata("ZiiQ", "text", 1000002, "content","这是一条测试信息");
+            //getToken参数1 corpid 企业id    参数2  应用秘钥
+            //String token = swx.getToken("ww8dad70e28c6446b8","3H5uzjWkB80kpwP0Qj2rWeKyfZaY1ZiPOXFLUGOb_Fg");
+            String token = swx.getToken("ww8dad70e28c6446b8","TAJOe8BrNxJttL2w_74va6nSKB07pRdtTGSLWuC_2EA");
+            String postdata = swx.createpostdata("xianingshuai", "text", 1000002, "content","这是一条测试信息");
             String resp = swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE,(new WeChatUrlData()).getSendMessage_Url(), postdata, token);
             System.out.println("获取到的token======>" + token);
             System.out.println("请求数据======>" + postdata);
             System.out.println("发送微信的响应数据======>" + resp);
+
         }catch (Exception e) {
             e.getStackTrace();
         }
