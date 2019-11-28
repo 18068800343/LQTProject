cf2076e0692d7e7c1706c47c8c07e401efc768b0
夏宁 <1042449477@qq.com>
2019-11-28 16:34:54 +0800

    xn

--- src/main/java/com/ldxx/controller/QualityByPlateRealTimeProdocutionDataController.java
+++ src/main/java/com/ldxx/controller/QualityByPlateRealTimeProdocutionDataController.java
@@ -1,14 +1,16 @@
 package com.ldxx.controller;
 
-import java.util.List;
-
+import com.ldxx.bean.QualityByPlateRealTimeProdocutionData;
+import com.ldxx.dao.QualityByPlateRealTimeProdocutionDataDao;
+import com.ldxx.service.QualityByPlateRealTimeProdocutionDataService;
+import com.ldxx.vo.PeiBiVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.ldxx.bean.QualityByPlateRealTimeProdocutionData;
-import com.ldxx.service.QualityByPlateRealTimeProdocutionDataService;
+import javax.annotation.Resource;
+import java.util.List;
 /**
  * 逐盘拌合生产数据实时监控
  * @author hp
@@ -20,12 +22,26 @@ public class QualityByPlateRealTimeProdocutionDataController {
 	
 	@Autowired
 	private QualityByPlateRealTimeProdocutionDataService service;
-	
+
+	@Resource
+	private QualityByPlateRealTimeProdocutionDataDao dao;
+
 	
 	@RequestMapping("/getAllQualityByPlateRealTimeProdocutionData")
 	@ResponseBody
 	public List<QualityByPlateRealTimeProdocutionData> getAllQualityByPlateRealTimeProdocutionData(){
-		return service.getAllQualityByPlateRealTimeProdocutionData();
+		List<QualityByPlateRealTimeProdocutionData> list = service.getAllQualityByPlateRealTimeProdocutionData();
+		return list;
+	}
+
+
+	@RequestMapping("/getPeiBiVoListByTime")
+	@ResponseBody
+	public PeiBiVo getPeiBiVoListByTime(String beginTime,String endTime){
+		List<QualityByPlateRealTimeProdocutionData> list = dao.getPeiBiVoListByTime(beginTime,endTime);
+		PeiBiVo peiBiVo = new PeiBiVo();
+		peiBiVo  = peiBiVo.getPeiBiVoByPeiBiVoList(list);
+		return peiBiVo;
 	}
 
 }
