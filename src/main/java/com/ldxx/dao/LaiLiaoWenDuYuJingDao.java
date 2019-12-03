b57c763a18e0e6576fdbcb3b3ce9d04e2e87807f
liuzhenfeng <liuzhenfeng@163.com>
2019-12-03 17:25:03 +0800

    lzf

--- src/main/java/com/ldxx/dao/LaiLiaoWenDuYuJingDao.java
+++ src/main/java/com/ldxx/dao/LaiLiaoWenDuYuJingDao.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface  LaiLiaoWenDuYuJingDao {
 
 
-	List<SiteIncomingMaterialTempWarningVo> getAllLaiLiaoWenDu();
+	List<SiteIncomingMaterialTempWarningVo> getAllLaiLiaoWenDu(String luduanquanxian);
 
 
 	int addLaiLiaoWenDu(SiteIncomingMaterialTempWarningVo siteIncomingMaterialTempWarningVo);
