dbb24c9666e2ed570e72ccf8fd5d7dac85e23201
liuzhenfeng <liuzhenfeng@163.com>
2019-12-12 11:14:12 +0800

    lzf

--- src/main/java/com/ldxx/controller/SysRoadMgnController.java
+++ src/main/java/com/ldxx/controller/SysRoadMgnController.java
@@ -30,11 +30,11 @@ public class SysRoadMgnController {
 
     @RequestMapping("/getAllSysRoadMgn")//通过权限初始化路段（大写）
     public List<SysRoadMgn> getAllSysRoadMgn(HttpSession session,String  roadquanxain){
-		User user = (User) session.getAttribute("user");
 		String luduanquanxian="";
 		if(roadquanxain!=""&&roadquanxain!=null){
 			luduanquanxian=roadquanxain;
 		}else{
+			User user = (User) session.getAttribute("user");
 			luduanquanxian = user.getLuduanquanxian();
 		}
         return srmService.getAllSysRoadMgn(luduanquanxian);
