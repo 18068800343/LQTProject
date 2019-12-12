88fbf27542c9cba1f0833b123dafe94c2c7341c9
hp <hp@hp>
2019-12-12 15:10:11 +0800

    jzq

--- src/main/java/com/ldxx/controller/LaiLiaoWenDuYuJingController.java
+++ src/main/java/com/ldxx/controller/LaiLiaoWenDuYuJingController.java
@@ -28,9 +28,15 @@ public class LaiLiaoWenDuYuJingController {
 
     @RequestMapping("/getAllLaiLiaoWenDu")
     @ResponseBody
-    public List<SiteIncomingMaterialTempWarningVo> getAllLaiLiaoWenDu(HttpSession session) {
-        User user = (User) session.getAttribute("user");
-        String luduanquanxian = user.getLuduanquanxian();
+    public List<SiteIncomingMaterialTempWarningVo> getAllLaiLiaoWenDu(HttpSession session ,String  roadquanxain) {
+    	String luduanquanxian="";
+		if(roadquanxain!=null){
+			luduanquanxian=roadquanxain;
+		}else{
+			User user = (User) session.getAttribute("user");
+	        luduanquanxian = user.getLuduanquanxian();
+		}
+        
         return service.getAllLaiLiaoWenDu(luduanquanxian);
     }
 
