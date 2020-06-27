7713d00baa59e064f42075a3c68e679826baf416
xianing <1042449477@qq.com>
2020-06-27 16:39:53 +0800

    XN

--- src/main/java/com/ldxx/bean/SiteConstruction.java
+++ src/main/java/com/ldxx/bean/SiteConstruction.java
@@ -32,6 +32,16 @@ public class SiteConstruction {
 
 	private String roadName;
 
+	private Integer state;
+
+	public Integer getState() {
+		return state;
+	}
+
+	public void setState(Integer state) {
+		this.state = state;
+	}
+
 	public String getRoadName() {
 		return roadName;
 	}
