16f11e74f7640fbe8d7ceb776c65045f73cb4953
xianing <1042449477@qq.com>
2020-07-06 16:15:36 +0800

    XN

--- src/main/java/com/ldxx/weChat/Test.java
+++ src/main/java/com/ldxx/weChat/Test.java
@@ -6,10 +6,10 @@ public class Test {
         WeChatMsgSend swx = new WeChatMsgSend();
         try {
             //getToken参数1 corpid 企业id    参数2  应用秘钥
-            //String token = swx.getToken("ww8dad70e28c6446b8","3H5uzjWkB80kpwP0Qj2rWeKyfZaY1ZiPOXFLUGOb_Fg");
-            String token = swx.getToken("ww8dad70e28c6446b8","TAJOe8BrNxJttL2w_74va6nSKB07pRdtTGSLWuC_2EA");
-            String postdata = swx.createpostdata("xianingshuai", "text", 1000002, "content","这是一条测试信息");
-            String resp = swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE,(new WeChatUrlData()).getSendMessage_Url(), postdata, token);
+            String token = swx.getToken("ww8dad70e28c6446b8", "3H5uzjWkB80kpwP0Qj2rWeKyfZaY1ZiPOXFLUGOb_Fg");
+//            String token = swx.getToken("ww8dad70e28c6446b8","TAJOe8BrNxJttL2w_74va6nSKB07pRdtTGSLWuC_2EA");
+            String postdata = swx.createpostdata("XiaNing", "text", 1000002, "content", "这是一条测试信息");
+            String resp = swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE, (new WeChatUrlData()).getSendMessage_Url(), postdata, token);
             System.out.println("获取到的token======>" + token);
             System.out.println("请求数据======>" + postdata);
             System.out.println("发送微信的响应数据======>" + resp);
