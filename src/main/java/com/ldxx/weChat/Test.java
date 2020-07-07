c088d9e4a7c38421813538afd89cf3ea135d73d8
xianing <1042449477@qq.com>
2020-07-07 10:39:02 +0800

    XN

--- src/main/java/com/ldxx/weChat/Test.java
+++ src/main/java/com/ldxx/weChat/Test.java
@@ -9,7 +9,7 @@ public class Test {
 //            String token = swx.getToken("ww8dad70e28c6446b8", "3H5uzjWkB80kpwP0Qj2rWeKyfZaY1ZiPOXFLUGOb_Fg");
             String token = swx.getToken("ww8dad70e28c6446b8", "9vCyCQz7nT2cgcDjR-2bIab6jCNqaFilZS8_0ikEuLs");
             String postdata = swx.createpostdata("ziiq", "text", 1000003, "content", "这是一条测试信息");
-            String postdata1 = swx.createPostDataApp("XiaNing", "温度预警通知", "12月17日 16:16", "温度值", "40");
+            String postdata1 = swx.createPostDataApp("ZhenFeng", "温度预警通知", "12月17日 16:16", "温度值", "40");
             String resp = swx.post("utf-8", WeChatMsgSend.CONTENT_TYPE, (new WeChatUrlData()).getSendMessage_Url(), postdata1, token);
             System.out.println("获取到的token======>" + token);
             System.out.println("请求数据======>" + postdata1);
